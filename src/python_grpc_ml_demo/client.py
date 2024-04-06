35e097d3599afbe04b7e7c10a78be7c08e468717
binghao <ngbinghao@gmail.com>
2024-04-06 16:12:10 +0800

    update

--- src/python_grpc_ml_demo/client.py
+++ src/python_grpc_ml_demo/client.py
@@ -8,52 +8,6 @@ from lib.ml_predictions_pb2 import Image
 from lib.utils import *
 
 
-# def pil_to_proto(img: I) -> Image:
-    
-#     (i_width, i_height) = img.size
-    
-#     return Image(mode=img.mode,
-#                  width=i_width,
-#                  height=i_height,
-#                  data=img.tobytes()
-#                  )
-    
-# def frame_to_pil(frame) -> I:
-    
-#     return I.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-
-# def video_stream(filepath, size=(960, 540), yield_every=5, max_frames=None):
-    
-#     vid = cv2.VideoCapture(filepath)
-    
-#     assert vid.isOpened(), "Video file not found!"
-    
-#     cur_frame = 0
-    
-#     while True:
-        
-#         ret, f = vid.read()
-        
-#         cur_frame += 1
-        
-#         if (not ret) or (max_frames and (cur_frame >= max_frames)):
-            
-#             break
-        
-#         if yield_every & ((cur_frame % yield_every) == 0):
-            
-#             yield frame_to_pil(f).resize(size)
-            
-#     vid.release()
-    
-#     return
-
-# def proto_stream(frame_stream):
-    
-#     for f in frame_stream:
-        
-#         yield pil_to_proto(f)
-
 def run(stub):
     
     demo = gr.Interface(fn=lambda x: x, inputs = ["text"], outputs = ["text"])
@@ -67,21 +21,21 @@ if __name__ == "__main__":
         
         stub = MlPredictionsStub(channel)
         
-        # run(stub)
-        img = I.open("assets/cat_dog.jpg")
-        print(img)
+        run(stub)
+        # img = I.open("assets/cat_dog.jpg")
+        # print(img)
         
-        img_proto = pil_to_proto(img)
+        # img_proto = pil_to_proto(img)
         
-        res = stub.PredictSingleImage(img_proto)
+        # res = stub.PredictSingleImage(img_proto)
         
-        print(res)
+        # print(res)
         
-        vid_frame_stream = video_stream("assets/KFC.mp4")
+        # vid_frame_stream = video_stream("assets/KFC.mp4")
         
-        resp = stub.PredictMultipleImages(proto_stream(vid_frame_stream))
+        # resp = stub.PredictMultipleImages(proto_stream(vid_frame_stream))
         
-        for r in resp:
+        # for r in resp:
             
-            print(r)
+        #     print(r)
             
